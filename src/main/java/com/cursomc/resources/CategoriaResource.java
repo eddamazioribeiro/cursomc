c702f823ab3b428ea5816293a1f7a743fdc1b74a
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2018-10-21 15:08:08 -0300

    Listando todas categorias com DTO

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,9 @@ package com.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursomc.domain.Categoria;
+import com.cursomc.dto.CategoriaDTO;
 import com.cursomc.services.CategoriaService;
 
 @RestController
@@ -53,5 +57,13 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 		
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+		
+	}	
 
 }
--- src/main/java/com/cursomc/resources/exceptions/StandardError.java
+++ src/main/java/com/cursomc/resources/exceptions/StandardError.java
@@ -1,12 +1,15 @@
 package com.cursomc.resources.exceptions;
 
 import java.io.Serializable;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Integer status;
 	private String msg;
 	private Long timeStamp;
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
 	
 	public StandardError(int status, String msg, long timeStamp) {
 		super();
